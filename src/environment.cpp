cb539e968fcd34b97b26e9c4f6a09f1c74e60cba
kjetlan <kjetlan@users.noreply.github.com>
2020-04-24 17:20:24 +0200

    Tune clustering parameters for lidar data stream

--- src/environment.cpp
+++ src/environment.cpp
@@ -126,7 +126,7 @@ void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointCloud
 
 
     // Clusters obstacles
-    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(segmentCloud.first, 0.5, 10, 500);
+    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessorI->Clustering(segmentCloud.first, 0.4, 10, 800);
 
     // Render obstacle clusters
     int clusterId = 0;
